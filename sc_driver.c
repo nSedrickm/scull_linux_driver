dd92d4586b2e4594741be5e3fef2687bda2edb70
nsedrickm <nsedrick101@gmail.com>
2020-04-18 18:59:15 +0100

    driver and makefile

---
+++ sc_driver.c
@@ -0,0 +1,15 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_AUTHOR("Ngwa Sedrick Meh");
+int load_driver(void)
+{
+    printk(KERN_INFO "driver loaded\n");
+    return 0;
+}
+void unload_driver(void)
+{
+    printk(KERN_INFO "driver unloaded\n");
+}
+module_init(load_driver);
+module_exit(unload_driver);
