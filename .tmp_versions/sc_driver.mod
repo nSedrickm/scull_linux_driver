c2c513f237db050779c994e2fae347254620e6dc
nsedrickm <nsedrick101@gmail.com>
2020-04-19 07:31:59 +0100

    make

---
+++ .tmp_versions/sc_driver.mod
@@ -0,0 +1,3 @@
+/home/nsedrickm/Documents/scull_linux_driver/sc_driver.ko
+/home/nsedrickm/Documents/scull_linux_driver/sc_driver.o
+
--- sc_driver.c
+++ sc_driver.c
@@ -1,5 +1,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 
 MODULE_AUTHOR("Ngwa Sedrick Meh");
 int load_driver(void)
