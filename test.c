b07a78189cdf1157e8c74a4bd0246c83f9ac8bbe
nSedrickm <nsedrick101@gmail.com>
2020-04-19 18:43:12 +0100

    power-off

--- test.c
+++ test.c
@@ -1,13 +1,3 @@
-/**
- * @file   testebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
- * @version 0.1
- * @brief  A Linux user space program that communicates with the ebbchar.c LKM. It passes a
- * string to the LKM and reads the response from the LKM. For this example to work the device
- * must be called /dev/ebbchar.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
-*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<errno.h>
@@ -22,7 +12,7 @@ int main(){
    int ret, fd;
    char stringToSend[BUFFER_LENGTH];
    printf("Starting device test code example...\n");
-   fd = open("/dev/ebbchar", O_RDWR);             // Open the device with read/write access
+   fd = open("/dev/scull_driver", O_RDWR);             // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
